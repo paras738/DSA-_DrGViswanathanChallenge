b052cf50373b81e0db1013362f74dffb38ef597b
paras738 <tripathiparas033@gmail.com>
2026-08-22 10:00:16 +0530

    Time: 0 ms (100.00%) | Memory: 44 MB (94.04%) - LeetSync

---
+++ 1603-running-sum-of-1d-array/running-sum-of-1d-array.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[] runningSum(int[] nums) {
+        
+        int count=0;
+        for(int i=0;i<nums.length;i++)
+        {  
+            count=count+nums[i];
+            nums[i]=count;
+        }
+        return nums;
+    }
+}
