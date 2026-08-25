7b93ce46abbf53324e87933d82b18f988cfa8463
paras738 <tripathiparas033@gmail.com>
2026-08-25 14:58:38 +0530

    Time: 5 ms (61.32%) | Memory: 44.8 MB (46.63%) - LeetSync

---
+++ 2800-minimum-string-length-after-removing-substrings/minimum-string-length-after-removing-substrings.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int minLength(String s) {
+        
+        Stack<Character>st= new Stack<>();
+        
+        for(char ch:s.toCharArray())
+        {
+            if(!st.isEmpty()&&ch=='B'&&st.peek()=='A')
+            {
+                st.pop();
+            }
+            else if(!st.isEmpty()&&ch=='D'&&st.peek()=='C')
+            {
+                 st.pop();
+            }
+            else
+            {
+                st.push(ch);
+
+            }
+
+        }
+        return st.size();
+
+
+        
+
+
+
+
+    
+    }
+}
